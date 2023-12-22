bef9fcd23dcbc0a1fb0d0ec629e8fca64981a247
Betül aktoprak <betulaktoprak112@gmail.com>
2023-12-22 21:43:10 +0300

    Add project files.

---
+++ TechCareerWebApiTutorial/Controllers/CountriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechCareerWebApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetCountries()
+        {
+            string[] countries = new string[] { "AFGANİSTAN" ,"ALMANYA","AMERİKA BİRLEŞİK DEVLETLERİ","AMERİKAN SAMOASI (ABD)","ANDORRA","ANGOLA","ANGUİLLA (Birleşik Krallık)","ANTİGUA ve BARBUDA","ARJANTİN","ARNAVUTLUK","ARUBA (Hollanda)"," AZOR ADALARI (Portekiz)","AVUSTRALYA","AVUSTURYA","AZERBAYCAN","BAHAMALAR","BAHREYN","BANGLADEŞ","BARBADOS" };
+            return countries;
+        }
+    }
+}
---
+++ TechCareerWebApiTutorial/Controllers/CountriesValueController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechCareerWebApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesValueController : ControllerBase
+    {
+        [HttpGet]
+        public string GetCountries(int id)
+        {
+            string[] countries = new string[] { "AFGANİSTAN", "ALMANYA", "AMERİKA BİRLEŞİK DEVLETLERİ", "AMERİKAN SAMOASI (ABD)", "ANDORRA", "ANGOLA", "ANGUİLLA (Birleşik Krallık)", "ANTİGUA ve BARBUDA", "ARJANTİN", "ARNAVUTLUK", "ARUBA (Hollanda)", " AZOR ADALARI (Portekiz)", "AVUSTRALYA", "AVUSTURYA", "AZERBAYCAN", "BAHAMALAR", "BAHREYN", "BANGLADEŞ", "BARBADOS" };
+            if(id < 0 || id > countries.Length )
+            {
+                return "index hatası";
+            }
+            else
+            {
+                //id+countries.Length
+                return countries[id];
+            }
+            
+        }
+    }
+}
